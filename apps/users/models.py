4399b1aec97e10cb5cdf84275f1a6d788f89557d
Tim <timamasharipov47@gmail.com>
2021-10-09 19:55:04 +0300

    added users model.

---
+++ apps/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class ProfileRole(models.TextChoices):
+    USER = 'user', 'User'
+    MANAGER = 'manager', 'Manager'
+    COURIER = 'courier', 'Courier'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, default=ProfileRole.USER, choices=ProfileRole.choices)
+
+    class Meta:
+        db_table = 'profiles'
+
+    def __str__(self):
+        return f'{self.user} {self.role}'
