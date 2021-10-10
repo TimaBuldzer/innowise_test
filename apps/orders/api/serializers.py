30bf011f5862ff318014c70858f2957c4abc2c18
Tim <timamasharipov47@gmail.com>
2021-10-10 19:03:55 +0300

    added 'drf-yasg'. added Order apis.

--- apps/orders/api/serializers.py
+++ apps/orders/api/serializers.py
@@ -1,14 +1,44 @@
 from rest_framework import serializers
 from apps.orders import models as orders_models
+from apps.restaurants import models as restaurants_models
+from apps.restaurants.api import serializers as restaurant_serializers
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = orders_models.OrderItem
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    orderitem = OrderItemSerializer()
 
     class Meta:
         model = orders_models.Order
+        fields = '__all__'
+        extra_kwargs = {
+            'status': {'read_only': True},
+            'parent': {'read_only': True},
+            'profile': {'read_only': True},
+            'restaurant': {'read_only': True},
+        }
+
+    def create(self, validated_data):
+        user = self.context.get('request').user
+
+        parent_order = orders_models.Order.objects.create(
+            profile=user.profile,
+        )
+        for d in validated_data.get('dish_ids'):
+            dish = restaurants_models.Dish.objects.get(id=d.get('id'))
+            order = orders_models.Order.objects.create(
+                parent=parent_order,
+                profile=user.profile,
+                restaurant=dish.restaurant
+            )
+            orders_models.OrderItem.objects.create(
+                order=order,
+                dish=dish,
+                dish_name=dish.name,
+                dish_price=dish.price,
+                dish_quantity=d.get('quantity')
+            )
--- conf/urls.py
+++ conf/urls.py
@@ -19,6 +19,22 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +42,5 @@ urlpatterns = [
     path('', include('apps.orders.urls')),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
